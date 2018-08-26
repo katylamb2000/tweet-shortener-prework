f8a15ccee8c4213d94c77c01aada8d07f08cb522
katy@karstaway.com <katy@karstaway.com>
2018-08-26 14:22:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,3 +33,11 @@ def selective_tweet_shortener(tweet)
     tweet 
   end 
 end 
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140 
+    word_substituter(tweet)[0..140] + '...'
+  else 
+    tweet 
+  end 
+end
