bb413e7f1ebd0d52fc17ad14b1f6376caa72ef4f
katy@karstaway.com <katy@karstaway.com>
2018-08-26 14:07:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,10 @@
 # Write your code here.
 
 def dictionary 
-  words = 
+  words = {"hello" => 'hi',
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"
